1e74699e9153e4ded3487d8cefb467fc801afd84
y00rhighness <y00r@highness.org>
2023-09-27 18:27:11 +0000

    #23 - wip

---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform {
+    cloud {
+    organization = "y00rhighness-mtc-terransible"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+}
+provider "random" {
+  # Configuration options
+}
