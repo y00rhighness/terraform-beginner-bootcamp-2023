b1adb39480016e3f2af5e12cb7c7457fe23c40a9
y00rhighness <y00r@highness.org>
2023-09-28 21:32:03 +0000

    #29 - added all the things

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -25,18 +25,18 @@ variable "index_html_filepath" {
   description = "The file path for index.html"
   type        = string
 
-  # validation {
-  #   condition     = fileexists(var.index_html_filepath)
-  #   error_message = "The provided path for index.html does not exist."
-  # }
+  validation {
+    condition     = fileexists(var.index_html_filepath)
+    error_message = "The provided path for index.html does not exist."
+  }
 }
 
 variable "error_html_filepath" {
   description = "The file path for error.html"
   type        = string
 
-  # validation {
-  #   condition     = fileexists(var.error_html_filepath)
-  #   error_message = "The provided path for error.html does not exist."
-  # }
+  validation {
+    condition     = fileexists(var.error_html_filepath)
+    error_message = "The provided path for error.html does not exist."
+  }
 }
