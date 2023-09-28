e617a6735d29fff12123c3db5344028d626eaa2f
y00rhighness <y00r@highness.org>
2023-09-28 19:15:18 +0000

    #29 - wip

--- main.tf
+++ main.tf
@@ -12,8 +12,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
+
 module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
 }
